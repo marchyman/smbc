7021747e9c2cc17fd0a442f1ad65330248e04e15
Marco S Hyman <marc@snafu.org>
2023-09-18 19:11:46 -0700

    Update help info
    Reorder and mention look-around

--- smbc/SmbcHelp.swift
+++ smbc/SmbcHelp.swift
@@ -23,15 +23,15 @@ struct SmbcHelp: View {
             Text("""
                 Tap on the center image to show the next ride.
 
-                Long press on the center image to force schedule data refresh from the SMBC server.
+                Long press on the center image to refresh schedule data from the SMBC server.
+
+                Tap on "Sunday Morning Breakfast Club" text to open a browser window to the SMBC web site home page.
 
                 Tap on the Restaurants button to list the restaurants the club visits.  You can look at all restaurants or the restaurants in current rotation.
 
-                Tap on the Rides button to list the rides for a schedule year.  Different years may be selected.  The schedule for the year of the next ride will always be loaded when returning to the home screen.
+                Tap on the Rides button to list the rides for a schedule year. Other years may be selected from the list of rides. The schedule for the year of the next ride will always be loaded when returning to the home screen.
 
-                When viewing rides you can swipe left/right to look at the next/previous ride.
-
-                Tap on "Sunday Morning Breakfast Club" text to open a browser window to the SMBC web site home page.
+                When viewing rides you can swipe left/right to look at the next/previous ride. A tap on the restaurant location marker will invoke look-around when look-around data is available.
                 """)
             // swiftlint:enable line_length
         }
