ca12c9d7bb88bbae1841c07cc52d1ca465232520
Marco S Hyman <marc@snafu.org>
2024-04-27 17:02:07 -0700

    move refresh to viewstate

--- smbc/HomeView.swift
+++ smbc/HomeView.swift
@@ -12,14 +12,10 @@ import SwiftUI
 struct HomeView: View {
     @Environment(ProgramState.self) var state
     @Environment(\.colorScheme) private var colorScheme: ColorScheme
-    var viewState = ViewState.shared
+    @Bindable var viewState = ViewState.shared
 
     @State private var noMoreRides = false
     @State private var runRefreshTask = false
-    @State private var forceRefresh = false
-    @State private var refreshPresented = false
-    @State private var refreshError: String = ""
-    @State private var refreshErrorPresented = false
 
     var body: some View {
         NavigationStack {
@@ -39,8 +35,8 @@ struct HomeView: View {
                         }
                     }
                     .onLongPressGesture {
-                        refreshPresented = true
-                        forceRefresh = true
+                        viewState.refreshPresented = true
+                        viewState.forceRefresh = true
                         runRefreshTask.toggle()
                     }
                 Spacer()
@@ -53,67 +49,27 @@ struct HomeView: View {
                     HStack { SmbcHelp(); SmbcInfo() }
                 }
             }
-            .alert("Schedule Reload", isPresented: $refreshPresented) {
+            .alert("Schedule Reload", isPresented: $viewState.refreshPresented) {
                 // let the system provide the button
             } message: {
                 ScheduleReloadView()
             }
             .alert("Schedule Reload Error",
-                   isPresented: $refreshErrorPresented) {
+                   isPresented: $viewState.refreshErrorPresented) {
                 // let the system provide the button
             } message: {
-                ReloadErrorView(description: refreshError)
+                ReloadErrorView(description: viewState.refreshError)
             }
             .sheet(isPresented: $noMoreRides) {
                 NoMoreRideView()
                     .presentationDetents([.medium])
             }
             .task(id: runRefreshTask) {
-                await refresh()
+                await viewState.refresh(state)
             }
         }
     }
 
-    /// refresh model data from server when necessary
-    ///
-    /// Refresh rules:
-    /// 1) Refresh when asked due to a long press on SmbcImage
-    /// 2) Refresh when the current date is greater than the refreshDate
-    /// 3) Refresh for the following year when there are no more rides for the year
-    /// 4) Refresh when the current schedule is not loaded.  Handle the case where
-    ///   the current date is the end of the year
-    @MainActor
-    private func refresh() async {
-        @AppStorage(ASKeys.refreshDate) var refreshDate = Date()
-        @AppStorage(ASKeys.scheduleYear) var scheduleYear = bundleScheduleYear
-
-        var needRefresh = false
-        let today = Date()
-        var year = Calendar.current.component(.year, from: today)
-
-        if forceRefresh {
-            forceRefresh = false
-            needRefresh = true
-        } else if today > refreshDate {
-            needRefresh = true
-        } else if year == scheduleYear && state.rideModel.nextRide() == nil {
-            year += 1
-            needRefresh = true
-        } else {
-            let weekOfYear = Calendar.current.component(.weekOfYear, from: today)
-            if weekOfYear <= 52 && year != scheduleYear {
-                needRefresh = true
-            }
-        }
-        if needRefresh {
-            do {
-                try await state.refresh(year)
-            } catch let error {
-                refreshError = error.localizedDescription
-                refreshErrorPresented.toggle()
-            }
-        }
-    }
 }
 
 #Preview {
--- smbc/ViewState.swift
+++ smbc/ViewState.swift
@@ -20,8 +20,54 @@ enum TabItems {
 final class ViewState {
     var selectedTab: TabItems = .home
     var nextRide: ScheduledRide? = nil
+    var forceRefresh = false
+    var refreshPresented = false
+    var refreshError: String = ""
+    var refreshErrorPresented = false
 
     static let shared: ViewState = .init()
 
     private init() { }
+}
+
+extension ViewState {
+    // refresh model data from server when necessary
+    //
+    // Refresh rules:
+    // 1) Refresh when asked due to a long press on SmbcImage
+    // 2) Refresh when the current date is greater than the refreshDate
+    // 3) Refresh for the following year when there are no more rides for the year
+    // 4) Refresh when the current schedule is not loaded.  Handle the case where
+    //   the current date is the end of the year
+    func refresh(_ state: ProgramState) async {
+        @AppStorage(ASKeys.refreshDate) var refreshDate = Date()
+        @AppStorage(ASKeys.scheduleYear) var scheduleYear = bundleScheduleYear
+
+        var needRefresh = false
+        let today = Date()
+        var year = Calendar.current.component(.year, from: today)
+
+        if forceRefresh {
+            forceRefresh = false
+            needRefresh = true
+        } else if today > refreshDate {
+            needRefresh = true
+        } else if year == scheduleYear && state.rideModel.nextRide() == nil {
+            year += 1
+            needRefresh = true
+        } else {
+            let weekOfYear = Calendar.current.component(.weekOfYear, from: today)
+            if weekOfYear <= 52 && year != scheduleYear {
+                needRefresh = true
+            }
+        }
+        if needRefresh {
+            do {
+                try await state.refresh(year)
+            } catch let error {
+                refreshError = error.localizedDescription
+                refreshErrorPresented.toggle()
+            }
+        }
+    }
 }
